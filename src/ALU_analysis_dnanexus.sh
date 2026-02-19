6960bc84dc149962600be2c5db40ac40f68351de
isabelrem <irt99@protonmail.com>
2026-02-19 16:38:12 +0000

    Draft ALU insertion DNAnexus applet

---
+++ src/ALU_analysis_dnanexus.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+# Exit on error, print each command as it runs
+set -e -x -o pipefail
+
+# Download all inputs specified in dxapp.json
+dx-download-all-inputs --parallel
+
+# Make output directories
+mkdir -p ~/out/all_outputs
+
+# Download Docker image using hardcoded file ID
+scramble_docker_file_id=project-XXXXXXXXXX:file-XXXXXXXXXX
+dx download ${scramble_docker_file_id}
+
+# Get the filename and extract the image name from the tar manifest
+scramble_docker_image_file=$(dx describe ${scramble_docker_file_id} --name)
+echo "Docker image file: ${scramble_docker_image_file}"
+scramble_docker_image_file=${scramble_docker_image_file//:/-}
+scramble_docker_image_name=$(tar xfO "${scramble_docker_image_file}" manifest.json | sed -E 's/.*"RepoTags":\["?([^"]*)"?.*/\1/')
+echo "Docker image name: ${scramble_docker_image_name}"
+
+# Load the Docker image
+docker load < /home/dnanexus/"${scramble_docker_image_file}"
+
+# Extract sample ID from BAM filename
+filename=${bam_name}
+sample_id=$(echo ${filename} | grep -oP 'NGS[^_]+_\d+')
+echo "Sample ID: ${sample_id}"
+
+# Set up directories
+mkdir -p /home/dnanexus/reference
+mkdir -p /home/dnanexus/out/all_outputs
+
+# Unpack reference genome tar.gz
+tar -xzf ${reference_tar_path} -C /home/dnanexus/reference/
+
+# Find the reference fasta
+ref_fa=$(find /home/dnanexus/reference -name "*.fa" -o -name "*.fasta" | head -n 1)
+echo "Reference FASTA: ${ref_fa}"
+
+# Rename BAM/BAI to sample ID for use inside container
+cp ${bam_path} /home/dnanexus/${sample_id}.bam
+cp ${bai_path} /home/dnanexus/${sample_id}.bai
+
+# Build optional arguments
+bed_mount=""
+bed_arg=""
+if [ -n "${bed_path}" ]; then
+    bed_mount="-v ${bed_path}:/app/data/regions.bed"
+    bed_arg="--bed /app/data/regions.bed"
+fi
+
+verbose_arg=""
+if [ "${verbose}" = "true" ]; then
+    verbose_arg="--verbose"
+fi
+
+# Run the Scramble Docker container
+docker run --rm \
+    -v /home/dnanexus/${sample_id}.bam:/app/data/${sample_id}.bam \
+    -v /home/dnanexus/${sample_id}.bai:/app/data/${sample_id}.bai \
+    -v ${ref_fa}:/app/data/reference.fa \
+    -v ${ref_fa}.fai:/app/data/reference.fa.fai \
+    -v ${ref_fa}.nhr:/app/data/reference.fa.nhr \
+    -v ${ref_fa}.nin:/app/data/reference.fa.nin \
+    -v ${ref_fa}.nsq:/app/data/reference.fa.nsq \
+    -v /home/dnanexus/out/all_outputs:/app/output \
+    ${bed_mount} \
+    ${scramble_docker_image_name} \
+    --bam /app/data/${sample_id}.bam \
+    --bai /app/data/${sample_id}.bai \
+    --window ${window} \
+    --polyA_window ${polyA_window} \
+    --threshold ${threshold} \
+    --min_polyA_len ${min_polyA_len} \
+    --merge_gap ${merge_gap} \
+    --proximity ${proximity} \
+    ${bed_arg} \
+    ${verbose_arg}
+
+# Upload all outputs
+dx-upload-all-outputs --parallel
