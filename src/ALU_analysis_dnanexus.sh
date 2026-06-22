5d44c5b6f91188db4b8b9cae6f4ed92124259d58
isabelrem <irt99@protonmail.com>
2026-06-22 14:16:45 +0100

    change to applet to allow sequence search and scramble analysis of whole run folders

--- src/ALU_analysis_dnanexus.sh
+++ src/ALU_analysis_dnanexus.sh
@@ -5,44 +5,40 @@ set -e -x -o pipefail
 # Download all inputs specified in dxapp.json
 dx-download-all-inputs --parallel
 
-# Make scratch directory for docker output and named output folders for DNAnexus
-mkdir -p /home/dnanexus/scratch
+# Make output directory for docker output and named output folders for DNAnexus
 mkdir -p /home/dnanexus/reference
+mkdir -p /home/dnanexus/output
 mkdir -p ~/out/clusters_txt
 mkdir -p ~/out/vcf_gz
 mkdir -p ~/out/alu_vcf
-mkdir -p ~/out/alu_analysis_csv
 mkdir -p ~/out/alu_analysis_high_confidence_csv
-mkdir -p ~/out/alu_analysis_summary_txt
+mkdir -p ~/out/sequence_search_out
 
 # Download Docker image using hardcoded file ID
-scramble_docker_file_id=project-J1g3b9Q0BfbvfX94Y8xzx0zg:file-J6xyGqQ0BfbXP8XpBZ30q0VF
-dx download ${scramble_docker_file_id}
+alu_docker_file_id=project-J1g3b9Q0BfbvfX94Y8xzx0zg:file-J8pb9v00BfbqYXFvbfZjB9G4
+dx download ${alu_docker_file_id}
 
 # Get the filename and extract the image name from the tar manifest
-scramble_docker_image_file=$(dx describe ${scramble_docker_file_id} --name)
-echo "Docker image file: ${scramble_docker_image_file}"
-scramble_docker_image_file=${scramble_docker_image_file//:/-}
-scramble_docker_image_name=$(tar xfO "${scramble_docker_image_file}" manifest.json | sed -E 's/.*"RepoTags":\["?([^"]*)"?.*/\1/')
-echo "Docker image name: ${scramble_docker_image_name}"
+alu_docker_image_file=$(dx describe ${alu_docker_file_id} --name)
+echo "Docker image file: ${alu_docker_image_file}"
+alu_docker_image_file=${alu_docker_image_file//:/-}
+alu_docker_image_name=$(tar xfO "${alu_docker_image_file}" manifest.json | sed -E 's/.*"RepoTags":\["?([^"]*)"?.*/\1/')
+echo "Docker image name: ${alu_docker_image_name}"
 
 # Load the Docker image
-docker load < /home/dnanexus/"${scramble_docker_image_file}"
+docker load < /home/dnanexus/"${alu_docker_image_file}"
 
-# Extract sample ID from BAM filename
-sample_id=$(basename ${bam_name} | grep -oP 'NGS[^_]+_\d+')
-echo "Sample ID: ${sample_id}"
+# remove docker image file once image is loaded
+rm /home/dnanexus/"${alu_docker_image_file}"
 
 # Unpack reference genome tar.gz
 tar -xzf ${reference_tar_path} -C /home/dnanexus/reference/
+ls -la /home/dnanexus/reference/
 
 # Find the reference fasta
 ref_fa=$(find /home/dnanexus/reference -name "*.fa" -o -name "*.fasta" | head -n 1)
 echo "Reference FASTA: ${ref_fa}"
-
-# Copy BAM/BAI to sample ID filename for use inside container
-cp ${bam_path} /home/dnanexus/${sample_id}.bam
-cp ${bai_path} /home/dnanexus/${sample_id}.bai
+REF_DIR=$(dirname "$ref_fa")
 
 # Build optional arguments
 bed_mount=""
@@ -57,43 +53,38 @@ if [ "${verbose}" = "true" ]; then
     verbose_arg="--verbose"
 fi
 
-# Run the Scramble Docker container
+# Run the ALU_analysis docker container
 docker run --rm \
-    -v /home/dnanexus/${sample_id}.bam:/app/data/${sample_id}.bam \
-    -v /home/dnanexus/${sample_id}.bai:/app/data/${sample_id}.bai \
-    -v ${ref_fa}:/app/data/reference.fa \
-    -v ${ref_fa}.fai:/app/data/reference.fa.fai \
-    -v ${ref_fa}.nhr:/app/data/reference.fa.nhr \
-    -v ${ref_fa}.nin:/app/data/reference.fa.nin \
-    -v ${ref_fa}.nsq:/app/data/reference.fa.nsq \
-    -v /home/dnanexus/scratch:/app/output \
-    ${bed_mount} \
-    ${scramble_docker_image_name} \
-    --bam /app/data/${sample_id}.bam \
-    --bai /app/data/${sample_id}.bai \
-    --window ${window} \
-    --polyA_window ${polyA_window} \
-    --threshold ${threshold} \
-    --min_polyA_len ${min_polyA_len} \
-    --merge_gap ${merge_gap} \
-    --proximity ${proximity} \
-    ${bed_arg} \
-    ${verbose_arg}
+  -e DX_SECURITY_CONTEXT="$DX_SECURITY_CONTEXT" \
+  -e DX_APISERVER_HOST="$DX_APISERVER_HOST" \
+  -e DX_APISERVER_PROTOCOL="$DX_APISERVER_PROTOCOL" \
+  -e DX_APISERVER_PORT="$DX_APISERVER_PORT" \
+  -v $(realpath $REF_DIR/hs37d5.fa):/app/data/reference.fa \
+  -v $(realpath $REF_DIR/hs37d5.fa.fai):/app/data/reference.fa.fai \
+  -v $(realpath $REF_DIR/hs37d5.fa.nhr):/app/data/reference.fa.nhr \
+  -v $(realpath $REF_DIR/hs37d5.fa.nin):/app/data/reference.fa.nin \
+  -v $(realpath $REF_DIR/hs37d5.fa.nsq):/app/data/reference.fa.nsq \
+  -v $(pwd)/output:/app/output \
+  "$alu_docker_image_name" \
+  --dx_project_id "$dx_project_id"
 
 # Copy outputs to named DNAnexus output folders
-cp /home/dnanexus/scratch/${sample_id}.clusters.txt           ~/out/clusters_txt/
-cp /home/dnanexus/scratch/${sample_id}.vcf.gz                 ~/out/vcf_gz/
-cp /home/dnanexus/scratch/${sample_id}_ALU_analysis_summary.txt ~/out/alu_analysis_summary_txt/
+cp /home/dnanexus/output/*.clusters.txt           ~/out/clusters_txt/
+cp /home/dnanexus/output/*.vcf.gz                 ~/out/vcf_gz/
+if [ -f /home/dnanexus/output/*_bam_output_*.txt ]; then
+    cp /home/dnanexus/output/*_bam_output_*.txt ~/out/sequence_search_out
+fi
 
 # ALU VCF name depends on whether a BED file was provided
 if [ -n "${bed_path}" ]; then
-    cp /home/dnanexus/scratch/${sample_id}_specified_region_ALU_ins.vcf ~/out/alu_vcf/
+    cp /home/dnanexus/output/*_specified_region_ALU_ins.vcf ~/out/alu_vcf/
 else
-    cp /home/dnanexus/scratch/${sample_id}_ALU_ins.vcf ~/out/alu_vcf/
+    cp /home/dnanexus/output/*_ALU_ins.vcf ~/out/alu_vcf/
 fi
 
-cp /home/dnanexus/scratch/${sample_id}_ALU_analysis.csv       ~/out/alu_analysis_csv/
-cp /home/dnanexus/scratch/${sample_id}_ALU_analysis_high_confidence.csv ~/out/alu_analysis_high_confidence_csv/
+for f in /home/dnanexus/output/*_ALU_analysis_high_confidence.csv; do
+    [ -f "$f" ] && cp "$f" ~/out/alu_analysis_high_confidence_csv/
+done
 
 # Upload all outputs
 dx-upload-all-outputs --parallel
